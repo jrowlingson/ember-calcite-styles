077644ea9a777c1e7551c755e33c5176b0bbb704
Jack Rowlingson <jack.rowlingson.dev@gmail.com>
2021-07-21 15:47:40 -0400

    initial

--- index.js
+++ index.js
@@ -1,5 +1,41 @@
 'use strict';
 
+const concat = require('broccoli-concat')
+const funnel = require('broccoli-funnel')
+const merge = require('broccoli-merge-trees')
+const postcssFilter = require('./lib/postcss-filter')
+const { mv } = require('broccoli-stew')
+
 module.exports = {
+
   name: require('./package').name,
-};
+
+  postprocessTree(type, tree) {
+    if (type === 'css') {
+      const stylesWithAddonCss = merge(
+        [tree, funnel(`${__dirname}/app/styles`, { destDir: 'assets' })],
+        {
+          overwrite: true,
+        }
+      )
+      return postcssFilter(
+        merge(
+          [
+            stylesWithAddonCss,
+            mv(
+              concat(stylesWithAddonCss, {
+                inputFiles: ['assets/vendor.css'],
+                footerFiles: ['assets/ember-calcite-styles.css'],
+                outputFile: 'vendor.css',
+              }),
+              'assets'
+            ),
+          ],
+          { overwrite: true }
+        )
+      )
+    }
+    return tree
+  },
+
+}
